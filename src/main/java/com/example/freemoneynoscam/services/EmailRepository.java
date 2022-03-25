577a3b3e7526b79606b20048e2ff465f90dfc347
Toby-byte <tobiasbogenielsen@gmail.com>
2022-03-25 13:02:28 +0100

    noter skrevet

--- src/main/java/com/example/freemoneynoscam/services/EmailRepository.java
+++ src/main/java/com/example/freemoneynoscam/services/EmailRepository.java
@@ -9,7 +9,7 @@ public class EmailRepository {
         ArrayList<String> dbEmails = new ArrayList<>();
 
         try {
-
+            // forbindelse bliver oprettet til database
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/toby", "Tobias KEA", "Bl@kie2349");
 
             Statement statement = connection.createStatement();
@@ -20,7 +20,7 @@ public class EmailRepository {
                 String emails = resultSet.getString("emails");
                 dbEmails.add(emails);
             }
-            System.out.println(dbEmails.toString());
+            System.out.println(dbEmails);
         } catch (Exception e) {
             e.printStackTrace();
         }
